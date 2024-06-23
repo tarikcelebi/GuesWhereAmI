7df16b8eb82726168db896034a4f7db86b89c8d1
tarikcelebi <tarikcelebi97@gmail.com>
2024-06-23 16:07:43 +0300

    chatRoom created and its front design handled.

--- src/screens/ChatRoomPage.js
+++ src/screens/ChatRoomPage.js
@@ -1,21 +1,57 @@
+import { Feather } from "@expo/vector-icons";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 
-const ChatRoomPage = ({route}) => {
-
-    const { item } = route.params;
-    console.log(item);
+const ChatRoomPage = ({ route }) => {
+  const { item } = route.params;
+  console.log(item);
   return (
-    <View style={styles.container}>
-      <Text>ChatRoomPage</Text>
+    <View>
+      <View style={styles.chatContainer}>
+        <View style={styles.chatListContainer}>
+          <Text>ChatRoomPage</Text>
+        </View>
+        <View style={styles.typeHereContainer}>
+          <TextInput style={styles.textInput} placeholder="type here..." autoFocus={true} />
+          <TouchableOpacity>
+            <Feather name="send" size={hp(3)}/>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  chatContainer: {
     flex: 1,
-    justifyContent: "center",
+    margin: 5,
+  },
+  chatListContainer: {
+    height: hp("80%"),
+    borderWidth: 1,
+    marginBottom: 5,
+  },
+  typeHereContainer: {
+    marginBottom: 5,
+    height: hp("5%"),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderWidth: 1, 
+    borderRadius: hp("2%"), 
+    borderColor: 'gray', 
+    alignItems: "center",
+    paddingHorizontal: 10, 
+  },
+  textInput: {
+    height: hp("100%"),
+    width: wp("85%"),
+    borderRadius: hp("2%"), 
+    fontSize: hp(2),
   },
 });
 
